7ffa120e20a2972fcf980385dae7c7d2415f271b
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2022-04-20 14:43:12 +0900

    02_02 完了

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,4 +1,10 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+float4 main(float4 pos : POSITION) : SV_POSITION
 {
-	return pos;
+	const float PI = 3.141519;
+	float angle = PI * 0.25f;
+	float4 pos2 = pos;
+
+	pos2.x = pos.x * cos(angle) - pos.y * sin(angle);
+	pos2.y = pos.x * sin(angle) + pos.y * cos(angle);
+	return pos2;
 }
